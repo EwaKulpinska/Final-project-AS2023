319811c62e43156ce55becd16aa0ac8ffcbf9ea1
Ewa Kulpinska <eva.kulpinska@gmail.com>
2023-08-21 12:15:01 +0200

    Added Navbar.js basic construction + styling

---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+export default function Navbar() {
+  return (
+    <div className="navbar container">
+      <a href="#!" className="logo">
+        F<span>oo</span>diesHub
+      </a>
+      <div className="nav-links">
+        <a href="#!" className="active">
+          Home
+        </a>
+        <a href="#!">Recipies</a>
+        <a href="#!">Settings</a>
+      </div>
+    </div>
+  );
+}
