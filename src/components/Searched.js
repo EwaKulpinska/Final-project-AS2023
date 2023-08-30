dc3f6d87c5aefdcfabf0145f02d107f2a3f4ddbb
Ewa Kulpinska <eva.kulpinska@gmail.com>
2023-08-30 14:45:49 +0200

    Search navigate works

--- src/components/Searched.js
+++ src/components/Searched.js
@@ -44,7 +44,7 @@ function Searched() {
 
 const Grid = styled.div`
   display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(20rem, 1fr));
+  grid-template-columns: repeat(auto-fit, minmax(15rem, 1fr));
   grid-gap: 3rem;
 `;
 
--- src/pages/Cuisine.js
+++ src/pages/Cuisine.js
@@ -47,7 +47,7 @@ function Cuisine() {
 
 const Grid = styled(motion.div)`
   display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(20rem, 1fr));
+  grid-template-columns: repeat(auto-fit, minmax(15rem, 1fr));
   grid-gap: 3rem;
 `;
 
