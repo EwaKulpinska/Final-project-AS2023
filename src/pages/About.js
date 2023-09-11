581b5f1f2f5a639228253ec7c4f50933535bca2a
Ewa Kulpinska <eva.kulpinska@gmail.com>
2023-09-11 11:56:38 +0200

    Added styling to About page

--- src/pages/About.js
+++ src/pages/About.js
@@ -7,7 +7,49 @@ export default function About() {
       initial={{ opacity: 0 }}
       exit={{ opacity: 0 }}
       transition={{ duration: 0.7 }}>
-      <h3>About Us</h3>
+      <form className="form">
+        <h4 className="header">Our newsletter</h4>
+        {/* name */}
+        <div className="form-row">
+          <label htmlFor="name" className="form-label">
+            Name
+          </label>
+          <input
+            className="form-input"
+            type="text"
+            name="name"
+            id="name"
+            defaultValue="John"
+          />
+        </div>
+        {/* lastName */}
+        <div className="form-row">
+          <label htmlFor="lastName">Last name</label>
+          <input
+            className="form-input"
+            type="text"
+            name="lastName"
+            id="lastName"
+            defaultValue="Doe"
+          />
+        </div>
+        {/* email */}
+        <div className="form-row">
+          <label htmlFor="email" className="form-label">
+            E-mail
+          </label>
+          <input
+            className="form-input"
+            type="text"
+            name="email"
+            id="email"
+            defaultValue="test@test.com"
+          />
+        </div>
+        <button className="form-btn" type="submit">
+          Submit
+        </button>
+      </form>
     </motion.div>
   );
 }
